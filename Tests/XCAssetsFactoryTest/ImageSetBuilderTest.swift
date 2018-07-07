11da1a6060a7af5b7003d20816077c9210286f76
Marcus Wu <marcus.wu0814@gmail.com>
2018-07-08 05:43:25 +0800

    Add a test case to cover worst case

--- Tests/XCAssetsFactoryTest/ImageSetBuilderTest.swift
+++ Tests/XCAssetsFactoryTest/ImageSetBuilderTest.swift
@@ -6,24 +6,11 @@ import PathKit
 
 class ImageSetBuilderTest: XCTestCase {
     
-    
-    override func setUp() {
-        super.setUp()
-        
-        TestEnv.setUp()
-        Grouped().execute()
-    }
-    
-    func test__givenXCAssetesPathAndImageDirPath__shouldMoveImageDirIntoXCAssetsToBeImageAssets() {
-        let sut = ImageSetBuilder(with: TestEnv.xcassetsPath)
-        sut.build(from: TestEnv.tempDir + Path("ic_a"))
-        
-        
-        let imageNamed = "ic_a"
-        let expectedImageSet = TestEnv.xcassetsPath + Path("ic_a.imageset")
-        let expectedContentJson = expectedImageSet + Path("Contents.json")
-        let expectedContentJsonString =
-        """
+    let imageNamed = "ic_a"
+    let expectedImageSet = TestEnv.xcassetsPath + Path("ic_a.imageset")
+    lazy var expectedContentJson = expectedImageSet + Path("Contents.json")
+    let expectedContentJsonString =
+    """
         {
           "images" : [
             {
@@ -48,9 +35,36 @@ class ImageSetBuilderTest: XCTestCase {
           }
         }
         """
-        let expected1xImage = expectedImageSet + Path(imageNamed + ".png")
-        let expected2xImage = expectedImageSet + Path(imageNamed + "@2x.png")
-        let expected3xImage = expectedImageSet + Path(imageNamed + "@3x.png")
+    lazy var expected1xImage = expectedImageSet + Path(imageNamed + ".png")
+    lazy var expected2xImage = expectedImageSet + Path(imageNamed + "@2x.png")
+    lazy var expected3xImage = expectedImageSet + Path(imageNamed + "@3x.png")
+    
+    override func setUp() {
+        super.setUp()
+        
+        TestEnv.setUp()
+        Grouped().execute()
+    }
+    
+    func test__givenXCAssetesPathAndImageDirPath__shouldMoveImageDirIntoXCAssetsToBeImageAssets() {
+        let sut = ImageSetBuilder(with: TestEnv.xcassetsPath)
+        sut.build(from: TestEnv.tempDir + Path("ic_a"))
+       
+        XCTAssertTrue(expectedImageSet.exists)
+        XCTAssertTrue(expectedContentJson.exists)
+        XCTAssertEqual(expectedContentJsonString, try? expectedContentJson.read())
+        XCTAssertTrue(expected1xImage.exists)
+        XCTAssertTrue(expected2xImage.exists)
+        XCTAssertTrue(expected3xImage.exists)
+    }
+    
+    func test__whenImageAssetsAlreadyExists__shouldMoveImageDirToCoverOldImageAssets() {
+        XCTAssertFalse(expectedImageSet.exists)
+        try? expectedImageSet.mkdir()
+        XCTAssertTrue(expectedImageSet.exists)
+        
+        let sut = ImageSetBuilder(with: TestEnv.xcassetsPath)
+        sut.build(from: TestEnv.tempDir + Path("ic_a"))
         
         XCTAssertTrue(expectedImageSet.exists)
         XCTAssertTrue(expectedContentJson.exists)
